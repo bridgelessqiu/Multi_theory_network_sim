ae877efd9da10e7dbe0d46cdaf1c20fd88c4a247
bridgelessqiu <zq5au@virginia.edu>
2020-09-30 21:39:59 -0400

    density function updated

--- mms/threshold.py
+++ mms/threshold.py
@@ -261,87 +261,65 @@ def correlate_threshold_fraction(A, B, T, k, r = 0, hub = False, clust = False):
 
 ###########################################################################################
 
-# def correlate_threshold_prob(A, B, prob, k, r = 0, hub = False, clust = False):
-#     """
-#         Description
-#         -----------
-#         This function simulate the spread of multiple contigions on a single network
-#         where each contagion has 2 states (0 or 1).
-#         Contagions are not interrelated.
+def correlate_threshold_density(A, B, T, d, k):
+    """
+        Description
+        -----------
+        This function simulate the spread of multiple contigions on a single network
+        where each contagion has 2 states (0 or 1).
+        Contagions are not interrated as described by the second model.
 
-#         Parameters
-#         ----------
-#         A: numpy matrix, int {0, 1}
-#           The adjacency matrix of G.
+        Parameters
+        ----------
+        A: numpy matrix, int {0, 1}
+          The adjacency matrix of G.
 
-#         B: numpy matrix, int {0, 1}
-#           The initial configuration matrix where $B_{vj}$ is the state value of 
-#           vertex v for contagion j.
+        B: numpy matrix, int {0, 1}
+          The initial configuration matrix where $B_{vj}$ is the state value of 
+          vertex v for contagion j.
 
-#         prob: function, returns float (0, 1)
-#           The probability function. Returns the probabiltiy of updating states to 1
+        T: numpy matrix, float [0, 1]
+          The threshold matrix where $T_{vj}$ is the threshold (floats) of vertex v for
+          contagion j.
 
-#         k: int
-#           The number of system iterations
-        
-#         r: float, optional
-#           The recovery probability. In each iteration, each vertex has a probability 
-#           r changing the state to 0 for each contigion.
+        d: numpy matrix, int
+          The density vector where $d_j$ is the density of the contagion $c_j$
 
-#         hub: boolean, optional
-#           If set true, prints out the indices of the iterations for which the top 10 
-#           hubs contract the contagions
-        
-#         clust: boolean, optional
-#           If set true, prints out the indices of the iterations for which the top 10 
-#           vertices with the highest clustering coefficients contract the contagions.
+        k: int
+          The number of system iterations
 
-#         Returns
-#         -------
-#         B: numpy matrix
-#           The final configuration
-#     """
+        Returns
+        -------
+        B: numpy matrix
+          The final configuration
+    """
+    # Compute the reciprocal 
+    d_bar = np.transpose( np.reciprocal(d.astype(float)) )
 
-#     # Check if A is indeed an adjacency matrix
-#     if not (mu.is_symmetric(A)):
-#         raise ValueError("The adjacency matrix is not symmetric")
-    
-#     # Make all 1s along the diagonal of A (since we are considering the closed neighborhood)
-#     np.fill_diagonal(A, 1)
+    # The number of contagions
+    c =  np.shape(T)[1]
 
-#     # Number of vertices
-#     n = np.shape(A)[0]
+    # k * 1 ones
+    one = np.ones((c, 1), dtype = 'float')
 
-#     # The inverse of diagonal degree matrix
-#     d = np.reciprocal(
-#         (A * np.ones((n, 1), dtype = 'float')).A1
-#     )
-#     D = np.diag(d)
+    for i in range(k):
+      B_last = B
 
-#     # The recovery probability
-#     recovery = False
-#     if r != 0:
-#         recovery = True
+      # Compute the totoal number of infections of each vertex
+      b = B * one
 
-#     # The main loop
-#     for i in range(k):
-#         # matrix operation
-#         B_last = B
-#         B = D * A * B_last - T
+      # Compute M
+      M = b * d_bar
+      M[M >= 1.0] = 1.0
+      M[M < 1.0] = 0.0
+      
+      B = A * M - T
 
-#         # update states
-#         B[B >= 0] = 1.0
-#         B[B < 0] = 0.0
+      # update states
+      B[B >= 0.0] = 1.0
+      B[B < 0.0] = 0.0
 
-#         # If a recovery probability is set
-#         if recovery:
-#             B[np.random.rand(*B.shape) < r] = 0.0
-
-#         # if fixed point
-#         if np.array_equal(B, B_last):
-#             print("A fixed point is reached at iteration {}".format(i))
-#             return B
-
-#     return B
-
-
+      # if fixed point
+      if np.array_equal(B, B_last):
+          print("A fixed point is reached at iteration {}".format(i))
+          return B
--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import mms.utility as mu
 import numpy as np
 
 if __name__ == "__main__":
-    A = np.matrix([[1, 2],[3, 4]])
-    C = np.matrix([[4, 4],[4, 4]])
-    B = np.matrix([[2, 2], [2, 2]])
-    B[A<C] = 0
-    print(B)
+    c = np.array([1,2,3])
+    c_bar = np.reciprocal(c.astype(float))
+    print(np.ones((2,1)))
