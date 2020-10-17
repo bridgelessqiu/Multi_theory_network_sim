c35c5531335d52ed1f2474cb80eda57b7ab97fae
bridgelessqiu <zq5au@virginia.edu>
2020-10-17 10:47:21 -0400

    main.py update - scipy sparse matrix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mms.threshold as mt
+from scipy import sparse
 import sys
 import time
 import mms.utility as mu
@@ -7,9 +8,9 @@ import numpy as np
 import sys
 
 """
+A and B are sparse
 mt.correlate_threshold_density(A, B, T, d, k)
 mt.correlate_threshold_count(A, B, T, W, k, r = 0)
-mt.correlate_threshold_fraction(A, B, T, k, r = 0)
 mt.isolate_threshold_count(A, B, T, k, r = 0)
 
 """
@@ -39,7 +40,7 @@ if __name__ == "__main__":
     p_list = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25]
 
     # list of all functions
-    list_of_func = [mt.correlate_threshold_density, mt.correlate_threshold_count, mt.correlate_threshold_fraction, mt.isolate_threshold_count]
+    list_of_func = [mt.correlate_threshold_density, mt.correlate_threshold_count, mt.isolate_threshold_count]
     f = list_of_func[1] # The function to select
     
     if network_type == 'real':
@@ -157,7 +158,7 @@ if __name__ == "__main__":
                 print("The time in seconds for the network:{} and the percentage: {} is {}".format(network_name, noise, end-start))
                 print()
 
-    if network_type == 'synthetic':
+    elif network_type == 'synthetic':
         if exp_type == 'increase_nodes':
             # directoy
             dirc = 'datasets/'+ network_type + '/' + exp_type + '/'
@@ -177,6 +178,7 @@ if __name__ == "__main__":
                 # Adjacency matrix #
                 ####################
                 A = nx.to_numpy_matrix(G) # Note: the order is G.nodes()
+                A = sparse.csr_matrix(A)
 
                 ####################
                 # Threshold matrix #
@@ -197,22 +199,21 @@ if __name__ == "__main__":
                 # Configuration matrix #
                 ########################
                 B = np.random.choice([0, 1], size = (n, c), p=[1.0 - p_infection, p_infection])
-                #B = np.random.randint(low = 0, high = 2, size = (n, c)) # Note: exclusion on the high value
-                #print(B)
+                B = sparse.csr_matrix(B)
 
                 ##################
                 # Density vector #
                 ##################
                 d = np.random.randint(low = 1, high = c+1, size = (c, 1)) # Note: exclusion on the high value
 
-                start = time.process_time() # The starting time
+                start = time.time() # The starting time
 
                 # The function call
                 f(A, B, T, W, k = num_iter)
 
-                end = time.process_time() # The ending time
+                end = time.time() # The ending time
 
-                print("RE network of size {} takes {} seconds".format(n, end-start))
+                print("RE network of size {} takes {} seconds".format(n, end - start))
                 print()
         elif exp_type == 'increase_edges':
             # directoy
